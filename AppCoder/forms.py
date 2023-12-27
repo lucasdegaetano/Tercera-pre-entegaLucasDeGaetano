64d3bdccd709ad6ec3733afa4e76740a338b9586
Toto341 <tomasdegaetano.dark@gmail.com>
2023-12-26 23:40:53 -0300

    Se incluyen los formuarios de form api

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -2,8 +2,19 @@ from django import forms
 
 class CategoriasFormulario(forms.Form):
 
-    #Especificar los campos
+    nombre = forms.CharField(max_length=40)
+
+class ProductosFormulario(forms.Form):
+
     nombre = forms.CharField(max_length=40)
     categoria = forms.IntegerField()
     precio = forms.DecimalField(max_digits=6, decimal_places=2)
-    stock = forms.IntegerField()
+    stock = forms.IntegerField()    
+
+class ClientesFormulario(forms.Form):
+
+    nombre = forms.CharField(max_length=40)
+    apellido = forms.CharField(max_length=40)
+    dni = forms.IntegerField()
+    email = forms.EmailField()
+    celular = forms.IntegerField()
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from models import Categorias
+from AppCoder.models import Categorias,Productos,Clientes
+from AppCoder.forms import CategoriasFormulario, ProductosFormulario,ClientesFormulario
 
 # Create your views here.
 def inicio(request):
@@ -15,29 +15,117 @@ def categorias(request):
 
 def productosFormulario(request):
     return render(request, "AppCoder/productosFormulario.html")
+
+"""
 def clientesFormulario(request):
-    return render(request, "AppCoder/clientesFormulario.html")
-
-
-def categoriasFormulario(request):
     
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        frmCategorias = categoriasFormulario(request.POST)
+        nombre = request.POST.get("nombre")
+        apellido = request.POST.get("apellido")
+        dni = request.POST.get("dni")
+        email = request.POST.get("email")
+        celular = request.POST.get("celular")
 
-        if frmCategorias.is_valid:
+        clientes = Clientes(nombre = nombre,
+                            apellido = apellido,
+                            dni = dni,
+                            email = email,
+                            celular = celular)
+        
+        clientes.save()            
+
+        return render(request, "AppCoder/inicio.html")        
+        
+    else:
+
+        frmClientes = ClientesFormulario()
+    
+    return render(request, "AppCoder/productosFormulario.html", {"frmClientes":frmClientes})
+
+"""
+
+def clientesFormulario(request):       
+
+    if request.method == "POST":
+
+        frmClientes = ClientesFormulario(request.POST)
+
+        if frmClientes.is_valid():
+
+            data = frmClientes.cleaned_data
+
+            nombre = data.get('nombre')
+            apellido = data.get('apellido')
+            dni = data.get('dni')
+            email = data.get('email')
+            celular = data.get('celular')
+
+            clientes = Clientes(nombre = nombre,
+                                apellido = apellido,
+                                dni = dni,
+                                email = email,
+                                celular = celular)
+            clientes.save()
+
+            return render(request, "AppCoder/inicio.html")      
+
+    else:
+
+        frmClientes = ClientesFormulario()
+    
+    return render(request, "AppCoder/clientesFormulario.html", {"frmClientes":frmClientes})
+
+
+def productosFormulario(request):       
+
+    if request.method == "POST":
+
+        frmProductos = ProductosFormulario(request.POST)
+
+        if frmProductos.is_valid():
+
+            data = frmProductos.cleaned_data
+
+            nombre = data.get('nombre')
+            categoria = data.get('categoria')
+            precio = data.get('precio')
+            stock = data.get('stock')
+
+            productos = Productos(nombre = nombre,
+                                  categoria = categoria,
+                                  precio = precio,
+                                  stock = stock)
+            productos.save()
+
+            return render(request, "AppCoder/inicio.html")      
+
+    else:
+
+        frmProductos = ProductosFormulario()
+    
+    return render(request, "AppCoder/productosFormulario.html", {"frmProductos":frmProductos})
+
+
+def categoriasFormulario(request):       
+
+    if request.method == "POST":
+
+        frmCategorias = CategoriasFormulario(request.POST)
+
+        if frmCategorias.is_valid():
 
             data = frmCategorias.cleaned_data
 
-            categoria= Categorias(  data['nombre'])
+            categoria = Categorias(nombre=data['nombre'])
             categoria.save()
 
             return render(request, "AppCoder/inicio.html")
         
-        else:
+    else:
 
-            frmCategorias = categoriasFormulario()
+        frmCategorias = CategoriasFormulario()
     
-    return render(request, "AppCoder/categorias/Formulario.html", {"frmCategorias":frmCategorias})
+    return render(request, "AppCoder/categoriasFormulario.html", {"frmCategorias":frmCategorias})
         
 
